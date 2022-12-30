d55d91e04be1c3260c45c9e0a909da27e330fe73
Joao Castro <cjoao.contato@gmail.com>
2022-12-29 23:09:00 -0300

    Adicionando DocStrings

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# python_oo
+# Python OO
+Aprendendo mais sobre Orientação à Objetos com Python, curso oferecido pela <a href='https://www.treinaweb.com.br/'>Treinaweb</a>, utilizando a linguagem Python.
--- carro.py
+++ carro.py
@@ -1,4 +1,6 @@
 class Carro():
+
+    '''Classe carro, Classe criada para criação de outros objetos do tipo carro'''
     def __init__(self, cor, qtd_portas, tipo_combustivel, qtd_combustivel, potencia, is_ligado, velocidade):
         self.cor = cor
         self.qtd_portas = qtd_portas
@@ -9,6 +11,7 @@ class Carro():
         self.velocidade = velocidade
 
     def abastecer(self, qtd_combustivel):
+        '''Método que incrementa a quantidade de combustível na variável qtd_combustivel'''
         self.qtd_combustivel += qtd_combustivel
         print(f'O carro possui: {self.qtd_combustivel}L de combustível')
 
